621a6153b3367a2c11d65d3167fdcfd9659b1c78
rev01c <sauve.9u1.peut@gmail.com>
2020-10-27 23:45:53 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 'use strict';
+/**
+ * 与えられた数に対応するフィボナッチ数を返す
+ * @param {Number} n 
+ */
+function fib(n) {
+    if (n === 0) {
+        return 0;
+    } else if (n === 1) {
+        return 1;
+    }
+    return fib(n -1) + fib(n - 2);
+}
+
+const length = 40;
+for (let i = 0; i <= length; i++) {
+    console.log(fib(i));
+}
